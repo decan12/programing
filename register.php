6d6b80db2f4613b3f63a00726a06f51f13af12c3
decan12 <101335307+decan12@users.noreply.github.com>
2022-03-11 00:10:11 +0700

    file login
    saya berhasil mengerjakan file login

---
+++ register.php
@@ -0,0 +1,58 @@
+<?php 
+
+include "function.php";
+
+if (isset($_POST['register'])) {
+    
+    if (register($_POST) > 0) {
+        echo "
+
+                <script>
+                alert('register berhasil ');
+                document.location.href = 'login.php';
+                </script>
+
+
+             ";
+    }else{
+        echo mysqli_error($coneksi);
+    }
+}
+
+
+
+
+ ?>
+
+
+
+<html>
+    <head>
+        <title>Halaman Login</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body class="q">
+        <div class="container">
+          <h1 class="w">Register</h1>
+            <form method="post" action="">
+                <label for="username">Username</label>
+                <br>
+                <input class="h" type="text" name="username" id="username">
+                <br>
+                <br>
+                <label>Password</label>
+                <br>
+                <input class="h" type="password" name="pasword" id="pasword">
+                 <br>
+                <label for="pasword2">Konfirmasi Password</label>
+                <br>
+                <input class="h" type="password" name="pasword2" id="pasword2">
+                <button type="submit" name="register">Register</button>
+                <p> Sudah punya akun?
+                  <a href="login.php">Login di sini</a>
+                </p>
+            </form>
+        </div>
+    </body>
+</html>
